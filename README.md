67d37ac00edca47974a7ae4eb875e457d4cf20e8
Your Name <you@example.com>
2018-01-10 16:43:50 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Weather NodeJS module
+
+### Installation
+
+`npm install -g kronver-weather`
+
+### Using
+
+`$ kronver-weather <City>`
---
+++ src/main.js
@@ -0,0 +1,58 @@
+#!/usr/bin/env node
+
+import axios from 'axios'
+
+const apiKey = '7ca53812a6dcac1b30f316f807354abd'
+
+/**
+ * Weather Data Class
+ */
+class WeatherData {
+	constructor(city, description, speed, temp) {
+		this.city = city
+		this.description = description
+		this.speed = speed
+		this.temp = temp
+	}
+}
+
+/**
+ * Get Weather Data
+ * @param {string} city 
+ */
+async function getWeatherData(city) {
+	let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}`
+	url += `&appid=${apiKey}&lang=en&units=metric`
+	let response = await axios.get(url).catch(e => console.log(e))
+	return new WeatherData(
+		city, 
+		response.data['weather'][0]['description'], 
+		response.data['wind']['speed'], 
+		response.data['main']['temp'])
+}
+
+/**
+ * Generate Weather Data
+ * @param {WeatherData} weatherData 
+ */
+function generateWeatherMessage(weatherData) {
+	return `Город ${weatherData.city}\n` +
+		`Сейчас на улице ${weatherData.description}\n` +
+		`Скорость ветра: ${Math.round(weatherData.speed)}\n` + 
+		`Температура воздуха: ${Math.round(weatherData.temp)}°C`
+}
+
+async function main() {
+	let city = process.argv[2]
+
+	if (! city) {
+		console.log('No city!')
+		process.exit(0)
+	}
+
+	let wd = await getWeatherData(city).catch(e => console.log(e))
+	let message = generateWeatherMessage(wd)
+	console.log(message)
+}
+
+main()
